b6831763fa396e3ea2c5a73dc3cfd7bf9432614b
duhezhe <laobahepijiu@163.com>
2021-09-12 19:20:55 +0800

    Day6-7

---
+++ src/leetcode/dfsAndbfs/Solution.java
@@ -0,0 +1,184 @@
+package leetcode.dfsAndbfs;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution {
+
+    /**
+     *给你一个坐标 (sr, sc) 表示图像渲染开始的像素值（行 ，列）和一个新的颜色值 newColor，让你重新上色这幅图像。
+     *
+     * 为了完成上色工作，从初始坐标开始，记录初始坐标的上下左右四个方向上像素值与初始坐标相同的相连像素点，接着再记录这四个方向上符合条件的像素点与他们对应四个方向上像素值与初始坐标相同的相连像素点，……，重复该过程。将所有有记录的像素点的颜色值改为新的颜色值。
+     *
+     * 最后返回经过上色渲染后的图像。
+     */
+
+    /**
+     * 广度优先算法
+     * @param s1
+     * @param s2
+     * @return
+     */
+    int[] dx = {1, 0, 0, -1};
+    int[] dy = {0, 1, -1, 0};
+
+    public int[][] floodFill_BFS(int[][] image, int sr, int sc, int newColor) {
+        int currColor = image[sr][sc];
+        if (currColor == newColor) {
+            return image;
+        }
+        int n = image.length, m = image[0].length;
+        Queue<int[]> queue = new LinkedList<int[]>();
+        queue.offer(new int[]{sr, sc});
+        image[sr][sc] = newColor;
+        while (!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            int x = cell[0], y = cell[1];
+            for (int i = 0; i < 4; i++) {
+                int mx = x + dx[i], my = y + dy[i];
+                if (mx >= 0 && mx < n && my >= 0 && my < m && image[mx][my] == currColor) {
+                    queue.offer(new int[]{mx, my});
+                    image[mx][my] = newColor;
+                }
+            }
+        }
+        return image;
+    }
+
+    /**
+     * 深度优先搜索
+     * @param image
+     * @param sr
+     * @param sc
+     * @param newColor
+     * @return
+     */
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int currColor = image[sr][sc];
+        if (currColor != newColor) {
+            dfs1(image, sr, sc, currColor, newColor);
+        }
+        return image;
+    }
+
+    public void dfs1(int[][] image, int x, int y, int color, int newColor) {
+        if (image[x][y] == color) {
+            image[x][y] = newColor;
+            for (int i = 0; i < 4; i++) {
+                int mx = x + dx[i], my = y + dy[i];
+                if (mx >= 0 && mx < image.length && my >= 0 && my < image[0].length) {
+                    dfs1(image, mx, my, color, newColor);
+                }
+            }
+        }
+    }
+
+    /**
+     * 给定一个包含了一些 0 和 1 的非空二维数组 grid 。
+     *
+     * 一个 岛屿 是由一些相邻的 1 (代表土地) 构成的组合，这里的「相邻」要求两个 1 必须在水平或者竖直方向上相邻。你可以假设 grid 的四个边缘都被 0（代表水）包围着。
+     *
+     * 找到给定的二维数组中最大的岛屿面积。(如果没有岛屿，则返回面积为 0 。)
+     */
+
+    /**
+     * 深度优先遍历法
+     * @param grid
+     * @return
+     */
+    public int maxAreaOfIsland_DFS(int[][] grid) {
+        int ans = 0;
+        for (int i = 0; i != grid.length; ++i) {
+            for (int j = 0; j != grid[0].length; ++j) {
+                ans = Math.max(ans, dfs2(grid, i, j));
+            }
+        }
+        return ans;
+    }
+
+    public int dfs2(int[][] grid, int cur_i, int cur_j) {
+        if (cur_i < 0 || cur_j < 0 || cur_i == grid.length || cur_j == grid[0].length || grid[cur_i][cur_j] != 1) {
+            return 0;
+        }
+        grid[cur_i][cur_j] = 0;
+        int[] di = {0, 0, 1, -1};
+        int[] dj = {1, -1, 0, 0};
+        int ans = 1;
+        for (int index = 0; index != 4; ++index) {
+            int next_i = cur_i + di[index], next_j = cur_j + dj[index];
+            ans += dfs2(grid, next_i, next_j);
+        }
+        return ans;
+    }
+
+    /**
+     * 深度优先+栈
+     * @param grid
+     * @return
+     */
+    public int maxAreaOfIsland_DFS_and_Stack(int[][] grid) {
+        int ans = 0;
+        for (int i = 0; i != grid.length; ++i) {
+            for (int j = 0; j != grid[0].length; ++j) {
+                int cur = 0;
+                Deque<Integer> stacki = new LinkedList<Integer>();
+                Deque<Integer> stackj = new LinkedList<Integer>();
+                stacki.push(i);
+                stackj.push(j);
+                while (!stacki.isEmpty()) {
+                    int cur_i = stacki.pop(), cur_j = stackj.pop();
+                    if (cur_i < 0 || cur_j < 0 || cur_i == grid.length || cur_j == grid[0].length || grid[cur_i][cur_j] != 1) {
+                        continue;
+                    }
+                    ++cur;
+                    grid[cur_i][cur_j] = 0;
+                    int[] di = {0, 0, 1, -1};
+                    int[] dj = {1, -1, 0, 0};
+                    for (int index = 0; index != 4; ++index) {
+                        int next_i = cur_i + di[index], next_j = cur_j + dj[index];
+                        stacki.push(next_i);
+                        stackj.push(next_j);
+                    }
+                }
+                ans = Math.max(ans, cur);
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * 广度优先算法
+     * @param grid
+     * @return
+     */
+    public int maxAreaOfIsland_BFS(int[][] grid) {
+        int ans = 0;
+        for (int i = 0; i != grid.length; ++i) {
+            for (int j = 0; j != grid[0].length; ++j) {
+                int cur = 0;
+                Queue<Integer> queuei = new LinkedList<Integer>();
+                Queue<Integer> queuej = new LinkedList<Integer>();
+                queuei.offer(i);
+                queuej.offer(j);
+                while (!queuei.isEmpty()) {
+                    int cur_i = queuei.poll(), cur_j = queuej.poll();
+                    if (cur_i < 0 || cur_j < 0 || cur_i == grid.length || cur_j == grid[0].length || grid[cur_i][cur_j] != 1) {
+                        continue;
+                    }
+                    ++cur;
+                    grid[cur_i][cur_j] = 0;
+                    int[] di = {0, 0, 1, -1};
+                    int[] dj = {1, -1, 0, 0};
+                    for (int index = 0; index != 4; ++index) {
+                        int next_i = cur_i + di[index], next_j = cur_j + dj[index];
+                        queuei.offer(next_i);
+                        queuej.offer(next_j);
+                    }
+                }
+                ans = Math.max(ans, cur);
+            }
+        }
+        return ans;
+    }
+}
---
+++ src/leetcode/subString/Solution.java
@@ -0,0 +1,58 @@
+package leetcode.subString;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    /**
+     * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int end = 0, start = 0; end < n; end++) {
+            char alpha = s.charAt(end);
+            if (map.containsKey(alpha)) {
+                start = Math.max(map.get(alpha), start);
+            }
+            ans = Math.max(ans, end - start + 1);
+            map.put(s.charAt(end), end + 1);
+        }
+        return ans;
+    }
+
+    /**
+     * 给你两个字符串 s1 和 s2 ，写一个函数来判断 s2 是否包含 s1 的排列。
+     *
+     * 换句话说，s1 的排列之一是 s2 的 子串 。
+     * @param s1
+     * @param s2
+     * @return
+     */
+    public boolean checkInclusion(String s1, String s2) {
+        int n = s1.length(), m = s2.length();
+        if (n > m) {
+            return false;
+        }
+        int[] cnt = new int[26];
+        for (int i = 0; i < n; ++i) {
+            --cnt[s1.charAt(i) - 'a'];
+        }
+        int left = 0;
+        for (int right = 0; right < m; ++right) {
+            int x = s2.charAt(right) - 'a';
+            ++cnt[x];
+            while (cnt[x] > 0) {
+                --cnt[s2.charAt(left) - 'a'];
+                ++left;
+            }
+            if (right - left + 1 == n) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
