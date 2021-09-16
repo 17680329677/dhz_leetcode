feb8b3497c807b7ce0627af38bc99fff021fb6a8
duhezhe <laobahepijiu@163.com>
2021-09-16 18:00:51 +0800

    DAY11

---
+++ src/leetcode/recall/Solution.java
@@ -0,0 +1,121 @@
+package leetcode.recall;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class Solution {
+
+    /**
+     * 给定两个整数 n 和 k，返回范围 [1, n] 中所有可能的 k 个数的组合。
+     * 你可以按 任何顺序 返回答案。
+     * @param n
+     * @param k
+     * @return
+     */
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (k <= 0 || n < k) {
+            return res;
+        }
+        // 从 1 开始是题目的设定
+        Deque<Integer> path = new ArrayDeque<>();
+        dfs(n, k, 1, path, res);
+        return res;
+    }
+
+    private void dfs(int n, int k, int begin, Deque<Integer> path, List<List<Integer>> res) {
+        // 递归终止条件是：path 的长度等于 k
+        if (path.size() == k) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        // 遍历可能的搜索起点
+        for (int i = begin; i <= n; i++) {
+            // 向路径变量里添加一个数
+            path.addLast(i);
+            // 下一轮搜索，设置的搜索起点要加 1，因为组合数理不允许出现重复的元素
+            dfs(n, k, i + 1, path, res);
+            // 重点理解这里：深度优先遍历有回头的过程，因此递归之前做了什么，递归之后需要做相同操作的逆向操作
+            path.removeLast();
+        }
+    }
+
+    /**
+     * 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> permute(int[] nums) {
+        // 首先是特判
+        int len = nums.length;
+        // 使用一个动态数组保存所有可能的全排列
+        List<List<Integer>> res = new ArrayList<>();
+
+        if (len == 0) {
+            return res;
+        }
+
+        boolean[] used = new boolean[len];
+        List<Integer> path = new ArrayList<>();
+
+        dfs(nums, len, 0, path, used, res);
+        return res;
+    }
+
+    private void dfs(int[] nums, int len, int depth,
+                     List<Integer> path, boolean[] used,
+                     List<List<Integer>> res) {
+        if (depth == len) {
+            // 3、不用拷贝，因为每一层传递下来的 path 变量都是新建的
+            res.add(path);
+            return;
+        }
+
+        for (int i = 0; i < len; i++) {
+            if (!used[i]) {
+                // 1、每一次尝试都创建新的变量表示当前的"状态"
+                List<Integer> newPath = new ArrayList<>(path);
+                newPath.add(nums[i]);
+
+                boolean[] newUsed = new boolean[len];
+                System.arraycopy(used, 0, newUsed, 0, len);
+                newUsed[i] = true;
+
+                dfs(nums, len, depth + 1, newPath, newUsed, res);
+                // 2、无需回溯
+            }
+        }
+    }
+
+    /**
+     * 给定一个字符串S，通过将字符串S中的每个字母转变大小写，我们可以获得一个新的字符串。返回所有可能得到的字符串集合。
+     * @param S
+     * @return
+     */
+    public List<String> letterCasePermutation(String S) {
+        List<StringBuilder> ans = new ArrayList();
+        ans.add(new StringBuilder());
+
+        for (char c: S.toCharArray()) {
+            int n = ans.size();
+            if (Character.isLetter(c)) {
+                for (int i = 0; i < n; ++i) {
+                    ans.add(new StringBuilder(ans.get(i)));
+                    ans.get(i).append(Character.toLowerCase(c));
+                    ans.get(n+i).append(Character.toUpperCase(c));
+                }
+            } else {
+                for (int i = 0; i < n; ++i)
+                    ans.get(i).append(c);
+            }
+        }
+
+        List<String> finalans = new ArrayList();
+        for (StringBuilder sb: ans)
+            finalans.add(sb.toString());
+        return finalans;
+    }
+}
