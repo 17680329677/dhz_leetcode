7a6f416cc902075fbad763763142e516ab2436d0
duhezhe <laobahepijiu@163.com>
2022-03-03 21:21:18 +0800

    二分查找2

--- src/kind/binary_search/Solution.java
+++ src/kind/binary_search/Solution.java
@@ -32,6 +32,46 @@ public class Solution {
         return high;
     }
 
+    /**
+     * 744. 寻找比目标字母大的最小字母
+     * 方法一：用set或长度为26的数组记录每个字母是否存在，然后一次判断target++是否存在
+     * 方法二：循环遍历找第一个大于target的字母即可
+     * 方法三：二分法，找到target的插入位置即可，如果最后low位置在最后 证明没有比target大的 返回0（取模）
+     * @param letters
+     * @param target
+     * @return
+     */
+    public char nextGreatestLetter(char[] letters, char target) {
+        int low = 0, high = letters.length;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (letters[mid] <= target) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return letters[low % letters.length];
+    }
+
+    /**
+     * 540. 有序数组中的唯一元素
+     * @param nums
+     * @return
+     */
+    public int singleNonDuplicate(int[] nums) {
+        int low = 0, high = nums.length - 1;
+        while (low < high) {
+            int mid = (high - low) / 2 + low;
+            if (nums[mid] == nums[mid ^ 1]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return nums[low];
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
         solution.mySqrt(4);
