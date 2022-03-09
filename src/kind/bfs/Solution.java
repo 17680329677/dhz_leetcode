e4f3a34cd035c49367cb5b680f5021513502b631
duhezhe <laobahepijiu@163.com>
2022-03-09 21:34:20 +0800

    广度优先遍历2

--- src/kind/bfs/Solution.java
+++ src/kind/bfs/Solution.java
@@ -2,7 +2,9 @@ package kind.bfs;
 
 import struct.TreeNode;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 /**
@@ -53,5 +55,133 @@ public class Solution {
             }
         }
         return queue1.isEmpty() && queue2.isEmpty();
+    }
+
+    /**
+     * 102. 二叉树的层次遍历
+     * 思路： 广度优先搜索
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null)
+            return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int curLevelSize = queue.size();
+
+            List<Integer> curLevel = new ArrayList<>();
+            for (int i = 0; i < curLevelSize; i++) {
+                TreeNode front = queue.poll();
+                curLevel.add(front.val);
+                if (front.left != null) {
+                    queue.offer(front.left);
+                }
+                if (front.right != null) {
+                    queue.offer(front.right);
+                }
+            }
+            res.add(curLevel);
+        }
+        return res;
+    }
+
+    /**
+     * 323. 无向图中连通分量的数量（你有一个包含 n 个节点的图。给定一个整数 n 和一个数组edges，其中edges[i] = [ai, bi]表示图中ai和bi之间有一条边。
+     * @param n
+     * @param edges
+     * @return
+     */
+    public int countComponents(int n, int[][] edges) {
+        // 第一步，根据信息构建连通图
+        List<Integer>[] adj = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            adj[i] = new ArrayList<>();
+        }
+        // 无向图，需要添加双向引用
+        for (int[] edge : edges) {
+            adj[edge[0]].add(edge[1]);
+            adj[edge[1]].add(edge[0]);
+        }
+
+        // 第二步， 开始广度遍历
+        int res = 0;
+        boolean[] visted = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (!visted[i]) {
+                bfs(adj, i, visted);
+                res++;
+            }
+        }
+        return res;
+    }
+
+    private void bfs(List<Integer>[] adj, int root, boolean[] visited) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(root);
+        visited[root] = true;
+
+        while (!queue.isEmpty()) {
+            Integer front = queue.poll();
+            // 获得队首节点的所有后继节点
+            List<Integer> successors = adj[front];
+            for (int successor : successors) {
+                if (!visited[successor]) {
+                    visited[successor] = true;
+                    queue.offer(successor);
+                }
+            }
+        }
+    }
+
+    /**
+     * 101. 对称二叉树（判断一棵二叉树是否对称）
+     * 方法一：递归
+     * @param root
+     * @return
+     */
+    public boolean isSymmetric(TreeNode root) {
+        return check(root, root);
+    }
+
+    private boolean check(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+            return true;
+        if (p == null || q == null)
+            return false;
+        return p.val == q.val && check(p.left, q.right) && check(p.right, q.left);
+    }
+
+    /**
+     * 101. 对称二叉树（判断一棵二叉树是否对称）
+     * 方法二：迭代，一般递归改迭代需借助队列
+     * @param root
+     * @return
+     */
+    public boolean isSymmetric2(TreeNode root) {
+        return check2(root, root);
+    }
+
+    private boolean check2(TreeNode u, TreeNode v) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(u);
+        queue.offer(v);
+        while (!queue.isEmpty()) {
+            u = queue.poll();
+            v = queue.poll();
+            if (u == null && v == null) {
+                continue;
+            }
+            if ((u == null || v == null) || u.val != v.val)
+                return false;
+            queue.offer(u.left);
+            queue.offer(v.right);
+
+            queue.offer(u.right);
+            queue.offer(v.left);
+        }
+        return true;
     }
 }
