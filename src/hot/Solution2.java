1a24cf2b27f2696b981d2f748a5d309b02d1a7f8
duhezhe <laobahepijiu@163.com>
2022-03-30 21:13:57 +0800

    hot200-day5

--- src/hot/Solution2.java
+++ src/hot/Solution2.java
@@ -1,8 +1,6 @@
 package hot;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Solution2 {
 
@@ -122,5 +120,64 @@ public class Solution2 {
             }
         }
         return left;
+    }
+
+    /**
+     * 51.N皇后问题
+     * @param n
+     * @return
+     */
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> res = new ArrayList<>();
+        int[] queens = new int[n];
+        Arrays.fill(queens, -1);
+        Set<Integer> columns = new HashSet<Integer>();
+        Set<Integer> diagonals1 = new HashSet<Integer>();
+        Set<Integer> diagonals2 = new HashSet<Integer>();
+        backtrack(res, queens, n, 0, columns, diagonals1, diagonals2);
+        return res;
+    }
+
+    private void backtrack(List<List<String>> res, int[] queens, int n, int row,
+                           Set<Integer> columns, Set<Integer> diagonals1, Set<Integer> diagonals2) {
+
+        if (row == n) {
+            List<String> board = generateBoard(queens, n);
+            res.add(board);
+        } else {
+            for (int i = 0; i < n; i++) {
+                if (columns.contains(i)) {
+                    continue;
+                }
+                int diagonal1 = row - i;
+                if (diagonals1.contains(diagonal1)) {
+                    continue;
+                }
+                int diagonal2 = row + i;
+                if (diagonals2.contains(diagonal2)) {
+                    continue;
+                }
+                queens[row] = i;
+                columns.add(i);
+                diagonals1.add(diagonal1);
+                diagonals2.add(diagonal2);
+                backtrack(res, queens, n, row + 1, columns, diagonals1, diagonals2);
+                queens[row] = -1;
+                columns.remove(i);
+                diagonals1.remove(diagonal1);
+                diagonals2.remove(diagonal2);
+            }
+        }
+    }
+
+    private List<String> generateBoard(int[] queens, int n) {
+        List<String> board = new ArrayList<>();
+        for (int i = 0; i < n; i ++) {
+            char[] row = new char[n];
+            Arrays.fill(row, '.');
+            row[queens[i]] = 'Q';
+            board.add(new String(row));
+        }
+        return board;
     }
 }
